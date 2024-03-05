377702ef3a08dcfa368b3044eb7fee5556ad9727
Joel Martínez <joel.mh112@mail.com>
2024-03-05 22:20:19 +0100

    [FEAT] added SSR with node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
-  adapter: vercel()
+  adapter: node({
+    mode: "standalone"
+  })
 });
