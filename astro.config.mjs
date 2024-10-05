b850dfb23995af8a59650b437fbf69b51682f080
Joel Martínez <joel.mh112@gmail.com>
2024-10-05 02:05:14 +0200

    [FEAT] updated astro + added vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
+  adapter: vercel({
+    imageService:true,
   })
 });
