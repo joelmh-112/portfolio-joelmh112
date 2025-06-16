ebf91803e9fe03e01b96ace8b9864328263c7783
Joel Martínez <joel.mh112@gmail.om>
2025-06-16 17:24:30 +0200

    [FEAT] upgraded to Astro 5.9.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
   adapter: vercel({
     imageService:true,
   })
